b77afd2b450477edb45cfc926fb48b5511895187
nair0612 <nairrohit0612@gmail.com>
2023-05-10 22:41:08 +0100

    Committing changes for pushing campaign form to solidtiy

--- src/app/campaign-form/campaign-form.component.ts
+++ src/app/campaign-form/campaign-form.component.ts
@@ -4,6 +4,14 @@ import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
 import { first } from 'rxjs/operators';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { CampaignService } from '../services/campaign.service';
+import { WalletService } from '../services/wallet.service';
+import { Injectable } from '@angular/core';
+import Web3 from 'web3';
+import { Contract } from 'web3-eth-contract';
+
+@Injectable({
+  providedIn: 'root'
+})
 
 @Component({
   selector: 'app-campaign-form',
@@ -13,13 +21,224 @@ import { CampaignService } from '../services/campaign.service';
 export class CampaignFormComponent implements OnInit {
   isDisabled: boolean = true
   camForm: FormGroup;
-  
+  public ethereum;
+  accountNo: string[];
+  web3: Web3;
+  contract: Contract;
+  contractAddress : string = '0xAA9B465A4d8d1511964Bd3111E809Fe1109c034a';
+  contractABI : any = [
+    {
+      "anonymous": false,
+      "inputs": [],
+      "name": "CampaignCreated",
+      "type": "event"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address payable",
+          "name": "_owner",
+          "type": "address"
+        },
+        {
+          "internalType": "string",
+          "name": "_title",
+          "type": "string"
+        },
+        {
+          "internalType": "string",
+          "name": "_description",
+          "type": "string"
+        },
+        {
+          "internalType": "uint256",
+          "name": "_goal",
+          "type": "uint256"
+        },
+        {
+          "internalType": "uint256",
+          "name": "_tokenPrice",
+          "type": "uint256"
+        },
+        {
+          "internalType": "uint256",
+          "name": "_numOfTokens",
+          "type": "uint256"
+        },
+        {
+          "internalType": "uint256",
+          "name": "_duration",
+          "type": "uint256"
+        },
+        {
+          "internalType": "string",
+          "name": "_profileImage",
+          "type": "string"
+        }
+      ],
+      "name": "createNewCrowdFunding",
+      "outputs": [
+        {
+          "internalType": "address",
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "name": "crowdfundingContracts",
+      "outputs": [
+        {
+          "internalType": "address",
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "campaignAddress",
+          "type": "address"
+        }
+      ],
+      "name": "getCampaignInfoByAddress",
+      "outputs": [
+        {
+          "components": [
+            {
+              "internalType": "address",
+              "name": "campaignAddress",
+              "type": "address"
+            },
+            {
+              "internalType": "address",
+              "name": "owner",
+              "type": "address"
+            },
+            {
+              "internalType": "string",
+              "name": "title",
+              "type": "string"
+            },
+            {
+              "internalType": "string",
+              "name": "description",
+              "type": "string"
+            },
+            {
+              "internalType": "uint256",
+              "name": "goal",
+              "type": "uint256"
+            },
+            {
+              "internalType": "uint256",
+              "name": "tokenPrice",
+              "type": "uint256"
+            },
+            {
+              "internalType": "uint256",
+              "name": "totalNumOfTokens",
+              "type": "uint256"
+            },
+            {
+              "internalType": "uint256",
+              "name": "remainNumOfTokens",
+              "type": "uint256"
+            },
+            {
+              "internalType": "uint256",
+              "name": "deadline",
+              "type": "uint256"
+            },
+            {
+              "internalType": "string",
+              "name": "profileImage",
+              "type": "string"
+            },
+            {
+              "internalType": "bool",
+              "name": "isActive",
+              "type": "bool"
+            },
+            {
+              "internalType": "bool",
+              "name": "terminated",
+              "type": "bool"
+            }
+          ],
+          "internalType": "struct CampaignFactory.FullCampaignInfo",
+          "name": "",
+          "type": "tuple"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "retrieveAllCampaignInfo",
+      "outputs": [
+        {
+          "components": [
+            {
+              "internalType": "address",
+              "name": "campaignAddress",
+              "type": "address"
+            },
+            {
+              "internalType": "address",
+              "name": "owner",
+              "type": "address"
+            },
+            {
+              "internalType": "string",
+              "name": "title",
+              "type": "string"
+            },
+            {
+              "internalType": "string",
+              "name": "description",
+              "type": "string"
+            },
+            {
+              "internalType": "string",
+              "name": "profileImage",
+              "type": "string"
+            }
+          ],
+          "internalType": "struct CampaignFactory.BasicCampaignInfo[]",
+          "name": "",
+          "type": "tuple[]"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    }
+  ];
+
+
   constructor(
     private _fb: FormBuilder,
     @Inject(MAT_DIALOG_DATA) public data: any,
     private _camService: CampaignService,
     private _dialogRef: MatDialogRef<CampaignFormComponent>,
+    private _walletService : WalletService
   ) { 
+    const {ethereum} = <any>window
+    this.ethereum = ethereum
     this.camForm = this._fb.group({
       fullName: '',
       campaignTitle: '',
@@ -35,7 +254,39 @@ export class CampaignFormComponent implements OnInit {
 
   ngOnInit() : void {
   this.camForm.patchValue(this.data);
+  if (this.ethereum) {
+    if (!this.checkWalletConnected()) {
+      this.connectToWallet();
+    }
+    else {
+      this.checkWalletConnected();
+    }
+    console.log(this.accountNo);
+    this.web3 = new Web3(this.ethereum);
+    try {
+      this.ethereum.enable().then(() => {
+        this.contract = new this.web3.eth.Contract(this.contractABI, this.contractAddress);
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  }
 }
+
+connectToWallet = async () => {
+  const accounts = await this._walletService.connectWallet();
+}
+
+checkWalletConnected = async () => {
+ const accounts = await this._walletService.checkWalletConnected();
+ if(accounts.length >0) {
+   this.accountNo = accounts[0];
+ }
+}
+
+// private loadContract(): void {
+//   this.contract = new this.web3.eth.Contract(this.contractABI, this.contractAddress);
+// }
 
 campaignForm = new FormGroup({
   fullName: new FormControl("", [Validators.required, Validators.minLength(2)]),
@@ -55,17 +306,38 @@ campaignSubmitted() {
     alert('Please fill all the empty fields')
   }
   else {
-      this._camService.addCampaign(this.camForm.value).subscribe({
-        next: (val: any) => {
-          alert('Campaign Added Successfully')
-          this._dialogRef.close();
-        },
-        error: (err: any) => {
-          console.error(err);
-        },
-      });
-    }
+    try {
+    this.checkWalletConnected();
+    const _owner = this.accountNo;
+    const _title = this.camForm.value.campaignTitle;
+    const _description = this.camForm.value.description;
+    const _goal = (this.camForm.value.goal)*10000000000;
+    const _tokenPrice = (this.camForm.value.tokenPrice)*10000000000;
+    const _numOfTokens = this.camForm.value.noTokens;
+    const _duration = 10;
+    const _profileImage = this.camForm.value.imageURL;
+
+    this.contract.methods
+  .createNewCrowdFunding(
+    _owner,
+    _title,
+    _description,
+    _goal,
+    _tokenPrice,
+    _numOfTokens,
+    _duration,
+    _profileImage
+  )
+  .send({ from: this.accountNo })
+  .then((receipt: any) => {
+    console.log(receipt);
+    this._dialogRef.close();
+  });
+  } catch(error) {
+  console.error(error);
   }
+  }
+}
 
 
 
