141f5a43038856b21850c7e67342dac3fa3521bb
nair0612 <nairrohit0612@gmail.com>
2023-05-24 11:16:46 +0100

    Revert "Develop"

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomePageComponent } from './home-page/home-page.component';
-import { AllCampaignComponent } from './all-campaign/all-campaign.component';
-import { MyCampaignComponent } from './my-campaign/my-campaign.component';
-import { MyInvestmentComponent } from './my-investment/my-investment.component';
-import { CampaignDetailsComponent } from './campaign-details/campaign-details.component';
 
-const routes: Routes = [
-  { path: '', redirectTo: '/Home', pathMatch: 'full'},
-  { path: 'Home', component: HomePageComponent },
-  { path: 'AllCampaign', component: AllCampaignComponent },
-  { path: 'MyCampaign', component: MyCampaignComponent },
-  { path: 'MyInvestment', component: MyInvestmentComponent },
-  { path: 'campaignDetails/:camapignId', component: CampaignDetailsComponent}
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
+  exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
